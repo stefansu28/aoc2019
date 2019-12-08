9c986a561cbb4c7583724ef371ae0f9ef1d9561a
Stefan Su <stefansu28@gmail.com>
2019-12-08 15:01:56 -0500

    day3 pt 2 WIP

--- day3/main.nim
+++ day3/main.nim
@@ -3,21 +3,64 @@ from strutils import parseInt, split, join
 from sequtils import map
 import sugar
 
+const mapSize = 64
+
 type
   Point = array[0..1, int]
-  Segment = (Point, Point)
+  Segment = (Point, Point, int)
   Wire = seq[Segment]
+  StepMapEntry = object
+    val: int
+    p: Point
+    next: ref StepMapEntry
+  StepMap = array[mapSize, ref StepMapEntry]
+
+proc hashPoint(p: Point): int =
+  return (abs(p[0] + 7789*p[1])) mod mapSize
+
+proc getSetMap(m: ptr StepMap, p: Point): (bool, int) =
+  var
+    prev: ref StepMapEntry = nil
+    node = m[hashPoint(p)]
+  while node != nil and node.p != p:
+    prev = node
+    node = node.next
+  if node.p == p:
+    return (true, node.val)
+  else:
+    return (false, 0)
+
+proc setStepMap(m: ptr StepMap, p: Point, steps: int): int =
+  var
+    prev: ref StepMapEntry = nil
+    node = m[hashPoint(p)]
+  while node != nil and node.p != p:
+    prev = node
+    echo node == nil
+    node = node.next
+    echo "hi"
+  if prev == nil and node == nil:
+    var newNode: ref StepMapEntry
+    new(newNode)
+    newNode.val = steps
+    newNode.p = p
+    newNode.next = nil
+    m[hashPoint(p)] = newNode
+    return steps
+  if node == nil:
+    # echo prev == nil
+    new(prev.next)
+    prev.next.val = steps
+    prev.next.p = p
+    prev.next.next = nil
+    return steps
+  else:
+    return node.val
 
 proc dist(p: Point): int {.inline.}=
   return abs(p[0]) + abs(p[1])
-
-# proc segMax(l: Segment): int {.inline.} =
-#   return max(dist(l[0]), dist(l[1])
-
-# proc segMin(l: Segment): int {.inline.} =
-#   return min(dist(l[0]), dist(l[1])
   
-proc intersect(l1: Segment, l2: Segment): (bool, Point) =
+proc intersect(l1: Segment, l2: Segment): (bool, Point, int) =
   # dir1 is the index that segment 1 changes
   # otherDir1 is the index that segment 1 is constant
   # same for 2's
@@ -29,7 +72,9 @@ proc intersect(l1: Segment, l2: Segment): (bool, Point) =
 
   # echo fmt"{l1}, {l2}"
 
-  var p: Point
+  var
+    p: Point
+    steps: int
   # segments are going in different directions
   if dir1 != dir2:
     p[otherDir1] = l1[0][otherDir1]
@@ -38,19 +83,22 @@ proc intersect(l1: Segment, l2: Segment): (bool, Point) =
       maxVal = max(l2[0][dir2], l2[1][dir2])
     if not (p[otherDir1] in minVal..maxVal):
       # echo "1 false"
-      return (false, p)
+      return (false, p, steps)
 
     p[otherDir2] = l2[0][otherDir2]
     minVal = min(l1[0][dir1], l1[1][dir1])
     maxVal = max(l1[0][dir1], l1[1][dir1])
     var intersected = p[otherDir2] in minVal..maxVal
     # echo fmt"2 {intersected}"
-    return (intersected, p)
+    if intersected:
+      steps = l1[2] + l2[2] + abs(p[dir1] - l1[0][dir1]) + abs(p[dir2] - l2[0][dir2])
+      echo fmt"l1:: {l1}, l2: {l2}, p: {p} steps: {steps}"
+    return (intersected, p, steps)
 
     # segments are going in same direction
   else:
     if l1[0][otherDir1] != l2[0][otherDir2]:
-      return (false, p)
+      return (false, p, steps)
     p[otherDir1] = l1[0][otherDir1]
     let
       min1 = min(l1[0][dir1], l1[1][dir1])
@@ -61,7 +109,7 @@ proc intersect(l1: Segment, l2: Segment): (bool, Point) =
     var possiblePoints: array[0..1, Point]
     if max1 < min2 or min1 > max2:
       # echo "3 false"
-      return (false, p)
+      return (false, p, steps)
     if max1 in min2..max2:
        possiblePoints[0][otherDir1] = p[otherDir1]
        possiblePoints[0][dir1] = max1
@@ -80,25 +128,53 @@ proc intersect(l1: Segment, l2: Segment): (bool, Point) =
     else:
       p = possiblePoints[1]
     # echo "4 true"
-    return (true, p)
+    steps = l1[2] + l2[2] + abs(p[dir1] - l1[0][dir1]) + abs(p[dir2] - l2[0][dir2])
+    return (true, p, steps)
+
+proc iterPoints(p1: Point, p2: Point, op: proc (p: Point): int{.closure, gcsafe, locks: 0.}
+): void =
+  var
+    dir = if p1[0] == p2[0]: 1 else: 0
+    p = p1
+  discard op(p)
+  while p != p2:
+    p[dir] += 1
+    discard op(p)
 
 proc createWire(input: openarray[string]): Wire =
   var
     p: Point = [0,0]
     wire: Wire
     i:int = 0
+    cumStep = 0
+    stepMap: StepMap
+
+  proc addPoint(p: Point): int =
+    setStepMap(addr(stepMap), p, cumStep)
   for s in input:
-    var next = p
-    var dir = input[i]
+    var
+      next = p
+      dir = input[i]
+      step: int
     i += 1
     case dir[0]
     of 'R':
       next[0] += parseInt(dir[1..^1])
-    of 'L': next[0] -= parseInt(dir[1..^1])
-    of 'U': next[1] += parseInt(dir[1..^1])
-    of 'D': next[1] -= parseInt(dir[1..^1])
+      step = next[0]
+    of 'L':
+      next[0] -= parseInt(dir[1..^1])
+      step = next[0]
+    of 'U':
+      next[1] += parseInt(dir[1..^1])
+      step = next[1]
+    of 'D':
+      next[1] -= parseInt(dir[1..^1])
+      step = next[1]
     else: raise newException(Exception, fmt"bad input {dir}")
-    wire.add((p, next))
+    iterPoints(p, next, addPoint)
+    var (_, pointStep) = getSetMap(addr(stepMap), p)
+    wire.add((p, next, pointStep))
+    cumStep += abs(step)
     p = next
   return wire
 
@@ -110,11 +186,11 @@ proc main() =
 
   for s1 in wire1:
     for s2 in wire2:
-      var (intersected, point) = intersect(s1, s2)
-      if intersected and (dist(point) < minInt or minInt == 0):
+      var (intersected, point, steps) = intersect(s1, s2)
+      if intersected and (steps < minInt or minInt == 0):
         # echo fmt"s1: {s1} s2: {s2}"
         # echo fmt"point: {point} dist: {dist(point)}"
-        minInt = dist(point)
+        minInt = steps
 
   echo minInt
 
